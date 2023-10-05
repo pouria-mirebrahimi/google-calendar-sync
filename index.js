026808787f29280ca52ec033769be385959d8249
Pouria Mirebrahimi <pouria.mirebrahimi@gmail.com>
2023-10-05 12:45:04 +0100

    fix: prepare for publish

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+function __export(m) {
+  for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+exports.__esModule = true;
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+__export(require('./dist/google'));
---
+++ lib/google/index.ts
@@ -0,0 +1,3 @@
+export * from './google.module';
+export * from './service/google.service';
+export * from './interface/google.interface';
