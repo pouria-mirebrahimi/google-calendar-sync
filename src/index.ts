ff9101149c662f25f5149bfa5efa3dac46ddbb2a
Pouria Mirebrahimi <pouria.mirebrahimi@gmail.com>
2023-10-05 08:21:52 +0100

    chore: setup, structure, and folders
    auth, token, calendar
    fix: prune
    fix: tsconfig

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import moment from 'moment-timezone';
-
-const dateTime = moment.tz(
-  'May 30th 2023 8:30PM',
-  'MMM Do YYYY h:mA',
-  'Asia/Tehran',
-);
-console.log(moment().startOf('minutes').from(dateTime));
